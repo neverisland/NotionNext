3a1ce4b2a36c1f088f33d345ca6158f2ac978fa8
neverisland <59460062+neverisland@users.noreply.github.com>
2025-12-12 14:49:50 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -28,7 +28,7 @@ const BLOG = {
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
 
-  // 其它复杂配置
+  // 其它复杂配置111
   // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
   ...require('./conf/comment.config'), // 评论插件
   ...require('./conf/contact.config'), // 作者联系方式配置
